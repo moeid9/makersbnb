f88f11e5154176b7bb70ce651bedebcf79eee7e6
terryhycheng <terryhycheng@gmail.com>
2023-02-14 17:35:11 +0000

    Added navbar for index & spaces pages. Added Logout function. Added logged-in-user name display. Boostrap on signup & login.

--- app.rb
+++ app.rb
@@ -1,35 +1,40 @@
-require 'sinatra/base'
-require 'sinatra/reloader'
+require "sinatra/base"
+require "sinatra/reloader"
 
-require './lib/database_connection.rb'
-require './lib/space.rb'
-require './lib/space_repository.rb'
+require "./lib/database_connection.rb"
+require "./lib/space.rb"
+require "./lib/maker.rb"
+require "./lib/space_repository.rb"
+require "./lib/maker_repository.rb"
 
-DatabaseConnection.connect('makersbnb_test')
+DatabaseConnection.connect("makersbnb_test")
 
 class Application < Sinatra::Base
   enable :sessions
 
   configure :development do
     register Sinatra::Reloader
-    also_reload 'lib/maker_repository'
-    
+    also_reload "lib/maker_repository"
   end
 
-  get '/' do
+  get "/" do
+    space_repo = SpaceRepository.new
+    maker_repo = MakersRepository.new
+    if session[:maker_id]
+      @maker = maker_repo.find(session[:maker_id])
+    end
     return erb(:index)
   end
 
-
-  get '/login' do
+  get "/login" do
     return erb(:login)
   end
 
-  get '/signup' do
+  get "/signup" do
     return erb(:signup)
   end
 
-  post '/signup' do
+  post "/signup" do
     repo = MakersRepository.new
     new_maker = Makers.new
     new_maker.name = params[:name]
@@ -44,38 +49,46 @@ class Application < Sinatra::Base
   # as body parameters, and find the user in the database
   # using the email. If the password matches, it returns
   # a success page.
-  post '/login' do
+  post "/login" do
     repo = MakersRepository.new
-    
+
     email = params[:email]
     password = params[:password]
 
     maker = repo.find_by_email(email)
-    
-    # This is a simplified way of 
-    # checking the password. In a real 
+
+    # This is a simplified way of
+    # checking the password. In a real
     # project, you should encrypt the password
     # stored in the database.
+
     if maker.password == password
       # Set the user ID in session
       session[:maker_id] = maker.id
-
-      return erb(:login_success)
+      redirect "/spaces"
     else
-      return''
+      return erb(:login)
     end
   end
 
-  get '/spaces' do
-    repo = SpaceRepository.new
-    @spaces = repo.all
+  get "/logout" do
+    session[:maker_id] = nil
+    redirect "/"
+  end
+
+  get "/spaces" do
+    space_repo = SpaceRepository.new
+    maker_repo = MakersRepository.new
+    if session[:maker_id]
+      @maker = maker_repo.find(session[:maker_id])
+    end
+    @spaces = space_repo.all
     return erb(:spaces)
   end
 
-  get '/spaces/:id' do
+  get "/spaces/:id" do
     repo = SpaceRepository.new
     @space = repo.find_by_id(params[:id])
     return erb(:space_single)
   end
-
 end
--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 require "rack/test"
-require_relative '../../app'
-require 'json'
+require_relative "../../app"
+require "json"
 
 describe Application do
   # This is so we can use rack-test helper methods.
@@ -17,34 +17,32 @@ describe Application do
   # one test suite for each set of related features),
   # you can duplicate this test file to create a new one.
 
-
-  context 'GET /' do
-    it 'should get the homepage' do
-      response = get('/')
+  context "GET /" do
+    it "should get the homepage" do
+      response = get("/")
 
       expect(response.status).to eq(200)
     end
   end
 
-
-  context 'GET /login' do
-    it 'should get the Login page' do
-      response = get('/login')
+  context "GET /login" do
+    it "should get the Login page" do
+      response = get("/login")
 
       expect(response.status).to eq(200)
-      expect(response.body).to include('<h1>Login</h1>')
+      expect(response.body).to include("<h1>Login</h1>")
       expect(response.body).to include('<form action="/login" method="POST">')
       expect(response.body).to include('<input type = "text" name = "email" />')
       expect(response.body).to include('<input type = "text" name = "password" />')
     end
   end
 
-  context 'GET /signup' do
-    it 'should get the Sign up page' do
-      response = get('/signup')
+  context "GET /signup" do
+    it "should get the Sign up page" do
+      response = get("/signup")
 
       expect(response.status).to eq(200)
-      expect(response.body).to include('<h1>Sign Up</h1>')
+      expect(response.body).to include("<h1>Sign Up</h1>")
       expect(response.body).to include('<form action="/signup" method="POST">')
       expect(response.body).to include('<input type = "text" name = "name" />')
       expect(response.body).to include('<input type = "text" name = "email" />')
@@ -52,120 +50,120 @@ describe Application do
     end
   end
 
-  context 'POST /login' do
-    it 'should get the Login page' do
+  context "POST /login" do
+    it "should get the Login page" do
       response = post(
-        '/login',
-        email: 'black_jack@email.com',
-        password: 'giant'
+        "/login",
+        email: "black_jack@email.com",
+        password: "giant",
       )
 
       expect(response.status).to eq(200)
     end
   end
 
-  context 'POST /signup' do
-    it 'should get the signup page' do
+  context "POST /signup" do
+    it "should get the signup page" do
       response = post(
-        '/signup',
-        name: 'Jack Daniels',
-        email: 'jack@email.com',
-        password: 'giantqwe'
+        "/signup",
+        name: "Jack Daniels",
+        email: "jack@email.com",
+        password: "giantqwe",
       )
 
       expect(response.status).to eq(200)
     end
   end
-  # GET /spaces 
-  context 'GET /spaces' do
-    it 'should return all the spaces' do
-      response = get('/spaces')
+  # GET /spaces
+  context "GET /spaces" do
+    it "should return all the spaces" do
+      response = get("/spaces")
 
       expect(response.status).to eq(200)
-      expect(response.body).to include('<h1>Book a Space</h1>')
+      expect(response.body).to include("<h1>Book a Space</h1>")
     end
   end
-  # GET /spaces/create 
-  xcontext 'GET /spaces/create' do
-    it 'should get the page to add a new space' do
-      response = get('/spaces')
+  # GET /spaces/create
+  xcontext "GET /spaces/create" do
+    it "should get the page to add a new space" do
+      response = get("/spaces")
 
       expect(response.status).to eq(200)
-      expect(response.body).to include('<h1>Add a new space</h1>')
+      expect(response.body).to include("<h1>Add a new space</h1>")
     end
   end
   # POST /spaces
-  xcontext 'POST /spaces' do
-    it 'should add a new space' do
-      response = post('/spaces')
+  xcontext "POST /spaces" do
+    it "should add a new space" do
+      response = post("/spaces")
 
       expect(response.status).to eq(200)
     end
   end
   # DELETE /spaces
-  xcontext 'POST /spaces' do
-    it 'should add a new space' do
-      response = delete('/spaces')
+  xcontext "POST /spaces" do
+    it "should add a new space" do
+      response = delete("/spaces")
 
       expect(response.status).to eq(200)
 
       repo = SpaceRepository.new
-      
+
       expect(repo.all.length).to eq 4
     end
   end
   # PUT /spaces
-  xcontext 'PATCH /spaces' do
-    it 'should update a space' do
-      response = patch('/spaces')
+  xcontext "PATCH /spaces" do
+    it "should update a space" do
+      response = patch("/spaces")
 
       expect(response.status).to eq(200)
     end
   end
 
   # GET /spaces/:id
-  context 'GET /spaces/:id' do
-    it 'should return a space with a given id' do
-      response = get('/spaces/1')
+  context "GET /spaces/:id" do
+    it "should return a space with a given id" do
+      response = get("/spaces/1")
 
       expect(response.status).to eq(200)
-      expect(response.body).to include('<p>Location: London</p>')
+      expect(response.body).to include("<p>Location: London</p>")
     end
   end
 
   # GET /spaces/:maker_id
-  xcontext 'GET /spaces/:maker_id' do
-    it 'should return a list of spaces with a given maker id' do
-      response = get('/spaces/2')
+  xcontext "GET /spaces/:maker_id" do
+    it "should return a list of spaces with a given maker id" do
+      response = get("/spaces/2")
 
       expect(response.status).to eq(200)
-      expect(response.body).to include('<p>Location: Leeds</p>')
-      expect(response.body).to include('<p>Name: Space B</p>')
-      expect(response.body).to include('<p>Name: Space C</p>')
+      expect(response.body).to include("<p>Location: Leeds</p>")
+      expect(response.body).to include("<p>Name: Space B</p>")
+      expect(response.body).to include("<p>Name: Space C</p>")
     end
   end
 
   # GET /spaces/:date
-  xcontext 'GET /spaces/:date' do
-    it 'should return a list of spaces with a given date' do
-      response = get('/spaces/2022-02-14')
+  xcontext "GET /spaces/:date" do
+    it "should return a list of spaces with a given date" do
+      response = get("/spaces/2022-02-14")
 
       expect(response.status).to eq(200)
-      expect(response.body).to include('<p>Location: Leeds</p>')
-      expect(response.body).to include('<p>Name: Space B</p>')
-      expect(response.body).to include('<p>Name: Space C</p>')
+      expect(response.body).to include("<p>Location: Leeds</p>")
+      expect(response.body).to include("<p>Name: Space B</p>")
+      expect(response.body).to include("<p>Name: Space C</p>")
     end
   end
 
   # GET /spaces/:location
-  xcontext 'GET /spaces/:location' do
-    it 'should return a list of spaces with a given location' do
-      response = get('/spaces/leeds')
+  xcontext "GET /spaces/:location" do
+    it "should return a list of spaces with a given location" do
+      response = get("/spaces/leeds")
 
       expect(response.status).to eq(200)
-      expect(response.body).to include('<p>Location: Leeds</p>')
-      expect(response.body).to include('<p>Name: Space B</p>')
-      expect(response.body).to include('<p>Name: Space C</p>')
+      expect(response.body).to include("<p>Location: Leeds</p>")
+      expect(response.body).to include("<p>Name: Space B</p>")
+      expect(response.body).to include("<p>Name: Space C</p>")
     end
   end
 end
