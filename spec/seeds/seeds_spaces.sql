58eadf26fa177170d7768eb2082a452d9f7856e4
Chris Leech <chrisleech@monzo.com>
2023-02-14 14:15:56 +0000

    removed code from seeds which generated users table with values

--- spec/seeds/seeds_spaces.sql
+++ spec/seeds/seeds_spaces.sql
@@ -1,7 +1,4 @@
-TRUNCATE TABLE makers, spaces RESTART IDENTITY CASCADE;
-
-INSERT INTO makers (name, email, password) VALUES('Terry', 'terry@terry.com', '1234');
-INSERT INTO makers (name, email, password) VALUES('Chris', 'chris@chris.com', 'password');
+TRUNCATE TABLE spaces RESTART IDENTITY CASCADE;
 
 INSERT INTO spaces (name, description, price, location, maker_id, date, available) VALUES('Space A', 'It is nice', 100, 'London', 1, '2022-02-13', TRUE);
 INSERT INTO spaces (name, description, price, location, maker_id, date, available) VALUES('Space B', 'It is awesome', 80, 'Leeds', 2, '2022-02-14', TRUE);
