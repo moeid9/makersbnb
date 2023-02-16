5552d7f4c6e25c89d74a32037617ab50a3a9c265
terryhycheng <terryhycheng@gmail.com>
2023-02-16 22:56:51 +0000

    Fixed truncation problem

--- spec/seeds/seeds_makers.sql
+++ spec/seeds/seeds_makers.sql
@@ -1,18 +1,41 @@
 -- (file: spec/seeds_makers.sql)
-
 -- Write your SQL seed here. 
-
 -- First, you'd need to truncate the table - this is so our table is emptied between each test run,
 -- so we can start with a fresh state.
 -- (RESTART IDENTITY resets the primary key)
+TRUNCATE TABLE makers,
+spaces,
+bookings RESTART IDENTITY;
 
-TRUNCATE TABLE makers RESTART IDENTITY CASCADE; -- replace with your own table name.
-
+-- replace with your own table name.
 -- Below this line there should only be `INSERT` statements.
 -- Replace these statements with your own seed data.
 -- Password: giant
-INSERT INTO makers (name, email, password) VALUES ('Jack Black', 'black_jack@email.com', '$2a$12$n1bS9MlnA3wv2SsrhqLW1OoTaCbq6jqeBAKtc2r8GVznLCKojpeqi');
+INSERT INTO
+  makers (name, email, password)
+VALUES
+  (
+    'Jack Black',
+    'black_jack@email.com',
+    '$2a$12$n1bS9MlnA3wv2SsrhqLW1OoTaCbq6jqeBAKtc2r8GVznLCKojpeqi'
+  );
+
 -- Password: monkey
-INSERT INTO makers (name, email, password) VALUES ('Micheal Jackson', 'dance@email.com', '$2a$12$p32LnXsl.5jCBo4fby06U.mLkkEWxVDBn6vHv.9W15TA75eCvvH9O');
+INSERT INTO
+  makers (name, email, password)
+VALUES
+  (
+    'Micheal Jackson',
+    'dance@email.com',
+    '$2a$12$p32LnXsl.5jCBo4fby06U.mLkkEWxVDBn6vHv.9W15TA75eCvvH9O'
+  );
+
 -- Password: Women<3
-INSERT INTO makers (name, email, password) VALUES ('Penelope Cruz', 'actor@email.com', '$2a$12$UKyyTXNARBHh/Qxcd56BZuHn5WdRh.06IjDgY5iB1oMPGK0E0ISWi');
+INSERT INTO
+  makers (name, email, password)
+VALUES
+  (
+    'Penelope Cruz',
+    'actor@email.com',
+    '$2a$12$UKyyTXNARBHh/Qxcd56BZuHn5WdRh.06IjDgY5iB1oMPGK0E0ISWi'
+  );
--- spec/seeds/seeds_spaces.sql
+++ spec/seeds/seeds_spaces.sql
@@ -1,4 +1,4 @@
-TRUNCATE TABLE spaces RESTART IDENTITY CASCADE;
+TRUNCATE TABLE bookings, spaces RESTART IDENTITY;
 
 INSERT INTO spaces (name, description, price, location, maker_id, date, available) VALUES('Space A', 'It is nice', 100, 'London', 1, '2022-02-13', TRUE);
 INSERT INTO spaces (name, description, price, location, maker_id, date, available) VALUES('Space B', 'It is awesome', 80, 'Leeds', 2, '2022-02-14', TRUE);
